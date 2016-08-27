c901761117ab9060fb200ce934d921ad1f5d1b8c
njk345 <njk@Nicks-MacBook-Retina.local>
2016-08-27 17:30:19 -0400

    Joystick work

--- joystick_test/joystick.h
+++ joystick_test/joystick.h
@@ -2,27 +2,28 @@
 #define JOYSTICK_Y_PIN A1
 
 typedef struct {
-    int x;
-    int y;
+    float x;
+    float y;
   } Position;
 
 typedef struct {
-    int left;
-    int right;
+    float left;
+    float right;
   } MotorValues;
 
 Position getPosition() {
     Position pos;
-    pos.x = analogRead(JOYSTICK_X_PIN)-512;
-    pos.y = analogRead(JOYSTICK_Y_PIN)-512;
+    pos.x = analogRead(JOYSTICK_X_PIN)/676.0;
+    pos.y = analogRead(JOYSTICK_Y_PIN)/676.0;
     return pos;
   }
 
 MotorValues getMotorValues() {
     Position pos = getPosition();
     MotorValues values;
-    values.left=((pos.y-pos.x)/8)+128;
-    values.right=((pos.y+pos.x)/8)+128;
+    values.left=pos.y*255;
+    values.right=pos.y*255;
     return values;
   }
+
 
